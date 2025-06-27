55d2fa819daee14261e4c40a335c545d36975432
Rselless <selles.rafael27@gmail.com>
2025-06-27 14:14:39 -0300

    fix: :bug: Adicionado Controller ao Module

--- src/funcionario/funcionario.module.ts
+++ src/funcionario/funcionario.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { TemaModule } from '../tema/tema.module';
-import { PostagemController } from './controllers/funcionario.controller';
-import { Funcionario, Postagem } from './entities/funcionario.entity';
-import { FuncionarioService, PostagemService } from './services/funcionario.service';
+import { FuncionarioController } from './controllers/funcionario.controller';
+import { Funcionario } from './entities/funcionario.entity';
+import { FuncionarioService } from './services/funcionario.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Funcionario]), ],
+  imports: [TypeOrmModule.forFeature([Funcionario])],
   providers: [FuncionarioService],
   controllers: [FuncionarioController],
   exports: [FuncionarioService],
 })
-export class PostagemModule {}
+export class FuncionarioModule {}
